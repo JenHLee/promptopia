5f4b884b1570ccdadfa6b8fcd46e39dbba4f6e3c
JenHLee <hj.jennie.lee@gmail.com>
2023-09-29 16:46:46 -0600

    Create User, Database and route to connect DB .env

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,27 @@
 import "@styles/globals.css";
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient"></div>
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
 
-        <main className="app">
+          <main className="app">
             {/* Put Nav in layout.js, it has to be used always  */}
             <Nav />
             {children}
-        </main>
-
+          </main>
+        </Provider>
       </body>
     </html>
   );
